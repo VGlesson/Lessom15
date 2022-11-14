bfbba4cbdfcd54daa4aa19d5f6eea09f90726ffc
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-11-14 20:00:21 +0300

    Добавьте файлы проекта.

---
+++ App1/GeomProgression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App1
+{
+    class GeomProgression : ISeries
+    {
+        double first;
+        double step;
+        public Geom(double first, double step) { this.first = first; this.step = step; }
+        public double GetElem(int n) { return first * Math.Pow(step, n - 1); }
+        public double GetSumm(int n) { return first * ((1 - Math.Pow(step, n)) / (1 - step)); }
+
+    }
+}
---
+++ App1/ISeries.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    interface ISeries
+    {
+        void SetStart(int х);
+        int GetNext();
+        void Reset();
+    }
+
+    class ArithProgression : ISeries
+    {
+        double first;
+        double step;
+        public ArithProgression(double first, double step) { this.first = first; this.step = step; }
+        public double GetElem(int n) { return first + step * (n - 1); }
+
+        public int GetNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetSumm(int n) { return n * (first + GetElem(n)) / 2; }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetStart(int х)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class GeomProgression : ISeries
+    {
+        double first;
+        double step;
+        public GeomProgression(double first, double step) { this.first = first; this.step = step; }
+        public double GetElem(int n) { return first * Math.Pow(step, n - 1); }
+
+        public int GetNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetSumm(int n) { return first * ((1 - Math.Pow(step, n)) / (1 - step)); }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetStart(int х)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
